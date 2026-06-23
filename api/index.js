f64fa091b4ab74cd2ebe371411ab855ec43fbb00
Ari Ardana <102762708+ariardana@users.noreply.github.com>
2026-06-23 23:20:05 +0700

    Enhance CORS proxy request handling
    Refactor request handling and improve URL correction.

--- api/index.js
+++ api/index.js
@@ -1,14 +1,22 @@
 const corsAnywhere = require('../lib/cors-anywhere');
 
-// Konfigurasi instance proxy cors-anywhere
 const proxy = corsAnywhere.createServer({
-  originWhitelist: [], // Biarkan kosong agar bisa diakses dari domain player lu
-  requireHeader: [],    // Dikosongkan agar player video m3u8/ts gak wajib kirim header aneh-aneh
+  originWhitelist: [], // Biar bisa ditembak dari domain pildun lu
+  requireHeader: [],    // Gak wajib kirim header aneh-aneh
   removeHeaders: ['cookie', 'cookie2']
 });
 
-// Export sebagai handler Serverless Function Vercel
 module.exports = (req, res) => {
-  // Alirkan request Vercel langsung ke dalam system cors-anywhere
+  // 1. Bersihkan routing bawaan Vercel jika ada
+  req.url = req.url.replace(/^\/api\/index\.js/, '');
+  
+  // 2. FIX SAKTI: Tambah balik slash yang dimaling sama sistem Vercel/Browser
+  if (req.url.startsWith('/https:/') && !req.url.startsWith('/https://')) {
+    req.url = req.url.replace('/https:/', '/https://');
+  } else if (req.url.startsWith('/http:/') && !req.url.startsWith('/http://')) {
+    req.url = req.url.replace('/http:/', '/http://');
+  }
+
+  // Lempar request yang udah rapi ke engine cors-anywhere
   proxy.emit('request', req, res);
 };
